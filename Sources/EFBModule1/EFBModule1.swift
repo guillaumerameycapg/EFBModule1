170033ddfa11ea4560384a9ef4e2da39c4dcfaac
Guillaume Ramey <guillaume.ramey@capgemini.com>
2022-01-10 16:53:38 +0100

    change icon

--- Sources/EFBModule1/EFBModule1.swift
+++ Sources/EFBModule1/EFBModule1.swift
@@ -1,5 +1,5 @@
 public struct EFBModule1 {
-  public private(set) var icon = "map.circle"
+  public private(set) var icon = "map"
   public private(set) var title = "Map"
   public private(set) var subtitle = "Shows a map of the area."
 
