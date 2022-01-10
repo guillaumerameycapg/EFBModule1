c7c811c28be0643349420a3f26ad66dbf5754b1f
Guillaume Ramey <guillaume.ramey@capgemini.com>
2022-01-10 14:55:24 +0100

    add view

--- Sources/EFBModule1/EFBModule1.swift
+++ Sources/EFBModule1/EFBModule1.swift
@@ -2,5 +2,6 @@ public struct EFBModule1 {
     public private(set) var title = "Module 1"
 
     public init() {
+      SwiftUIView()
     }
 }
---
+++ Sources/EFBModule1/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Guillaume Ramey on 10/01/2022.
+//
+
+import SwiftUI
+
+public struct SwiftUIView: View {
+  public var body: some View {
+        Text("Hello, module 1!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
