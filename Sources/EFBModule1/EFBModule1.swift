20159d7d0a9a0a4e8808a18bb23767d6edcc7496
Guillaume Ramey <guillaume.ramey@capgemini.com>
2022-01-13 12:07:06 +0100

    public vars

--- Sources/EFBModule1/EFBModule1.swift
+++ Sources/EFBModule1/EFBModule1.swift
@@ -1,9 +1,9 @@
 import EFBToolKit
 
 public struct EFBModule1: EFBModule {
-  public private(set) var icon = "map"
-  public private(set) var title = "Map"
-  public private(set) var subtitle = "Shows a map of the area."
+  public var icon = "map"
+  public var title = "Map"
+  public var subtitle = "Shows a map of the area."
 
   public init() {
   }
