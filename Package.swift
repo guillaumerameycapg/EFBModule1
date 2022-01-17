1b8cb76bcfcc867c507885dc051e4117fe311b9e
Guillaume Ramey <guillaume.ramey@capgemini.com>
2022-01-17 15:48:55 +0100

    add target dependency

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EFBModule1",
-            dependencies: []),
+            dependencies: ["EFBToolKit"]),
         .testTarget(
             name: "EFBModule1Tests",
             dependencies: ["EFBModule1"]),
